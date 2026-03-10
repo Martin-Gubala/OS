be02d2bf33d55a7c18b165afa82e35162fca1e57
Martin <martyg002@gmail.com>
2026-03-10 18:33:01 +0000

    Fix initialization of FreeSectorDescriptorStore and implement blocking_write_sector function

--- start64/diskdriver.c
+++ start64/diskdriver.c
@@ -25,6 +25,7 @@ void init_disk_driver(DiskDevice *dd, void *mem_start, unsigned long mem_length,
     
     FreeSectorDescriptorStore *store;
     g_dd = dd;
+    g_fsds=store;
     
     store = create_fsds();
     create_free_sector_descriptors(store, mem_start, mem_length);
@@ -33,10 +34,23 @@ void init_disk_driver(DiskDevice *dd, void *mem_start, unsigned long mem_length,
     g_write_queue = createBB(16);
     g_read_queue = createBB(16);
 
+    g_free_requests = createBB(16);
+    g_free_vouchers = createBB(16);
+
     pthread_create(&g_writer_tid, NULL, writer_thread_func, NULL); //still undefind
     pthread_create(&g_reader_tid, NULL, reader_thread_func, NULL);
 }
-void blocking_write_sector(SectorDescriptor *sd, Voucher **v);
+void blocking_write_sector(SectorDescriptor *sd, Voucher **v){
+    DriverRequest *req = aquire_reqest_blocking();
+    Voucher *new_v = acquire_voucher_blocking();
+
+    req->sd = sd;
+    req->voucher = new_v;
+    req->is_read = 0;
+
+    *v = new_v;
+    blockingWriteBB(g_write_queue, req);
+}
 int nonblocking_write_sector(SectorDescriptor *sd, Voucher **v);
 
 
